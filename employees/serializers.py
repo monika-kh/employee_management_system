e05484c06dad842380f05a9b8ad810583fce3761
monika-kh <khasdevmonika@gmail.com>
2020-01-14 18:41:53 +0530

    serializers.py updated.

--- employees/serializers.py
+++ employees/serializers.py
@@ -12,3 +12,8 @@ class RelationshipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Relationship
         fields = "__all__"
+
+    def to_representation(self, instance):                                           #represent employee's first_name, but for post method use employee id
+        rep = super(RelationshipSerializer, self).to_representation(instance)
+        rep['employee_id'] = instance.employee_id.first_name
+        return rep
