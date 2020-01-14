ae10a0865960b491b66de671b27a14c69a9116b9
monika-kh <khasdevmonika@gmail.com>
2020-01-14 18:41:03 +0530

    apps.py updated for signal.

--- employees/apps.py
+++ employees/apps.py
@@ -2,7 +2,16 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
+from .models import Employees
+from . signals import employee_salary, save_employee_salary
 
 class EmployeesConfig(AppConfig):
     name = 'employees'
+
+
+
+def ready(self):
+    post_save.connect(employee_salary, sender=Employees)
+    post_save.connect(save_employee_salary, sender=Employees)
